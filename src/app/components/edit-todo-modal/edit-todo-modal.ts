44e6bca603c45f7ecc95e518d8f8faa954c053a4
Júlia Rodrigues <heylavenderbee@gmail.com>
2026-05-01 11:59:36 -0300

    feat: add todo editing
    You can now edit to-dos!

---
+++ src/app/components/edit-todo-modal/edit-todo-modal.ts
@@ -0,0 +1,27 @@
+import { Component, inject, model } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MAT_DIALOG_DATA, MatDialogActions, MatDialogClose, MatDialogContent, MatDialogRef, MatDialogTitle, } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInput, MatFormField, MatInputModule, MatLabel } from "@angular/material/input";
+
+export interface DialogData {
+  id: number,
+  editedItem: string
+}
+
+@Component({
+  selector: 'app-edit-todo-modal',
+  imports: [MatButtonModule, MatDialogActions, MatDialogClose, MatDialogTitle, MatDialogContent, FormsModule, MatInput, MatFormField, MatLabel],
+  templateUrl: './edit-todo-modal.html',
+  styleUrl: './edit-todo-modal.css',
+})
+export class EditTodoModal {
+  readonly dialogRef = inject(MatDialogRef<EditTodoModal>);
+  readonly data = inject<DialogData>(MAT_DIALOG_DATA);
+  readonly editedItem = model(this.data.editedItem);
+
+  cancel(): void{
+    this.dialogRef.close();
+  }
+}
--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,6 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal, ChangeDetectorRef } from '@angular/core';
 import { Header } from '../header/header';
 import { FormsModule } from '@angular/forms';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { monoEdit } from '@ng-icons/mono-icons';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogClose, MatDialogContent, MatDialogRef, MatDialogTitle } from '@angular/material/dialog';
+import { EditTodoModal } from '../edit-todo-modal/edit-todo-modal';
 
 interface Todo {
     id: number,
@@ -9,22 +14,40 @@ interface Todo {
 
 @Component({
   selector: 'app-home',
-  imports: [Header, FormsModule],
+  imports: [Header, FormsModule, NgIcon, MatButtonModule, EditTodoModal],
   templateUrl: './home.html',
   styleUrl: './home.css',
+  viewProviders: [provideIcons({ monoEdit })],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Home {
+  private cdr = inject(ChangeDetectorRef);
+  readonly dialog = inject(MatDialog);
+  readonly editedItem = signal('');
+  readonly editedItemId = signal('');
   todo: string = "";
   todoList: Todo[] = [{id: 0, name: "tarefa 1"}, {id: 1, name: "tarefa 2"}];
-  todoListSize: number = 0;
-  a = {name: "hey"};
+  todoListSize: number = 0; 
 
   addTodoItem(todo: string){
     this.todoList.push({id: this.todoList.length, name: todo});
-    console.log(this.todoList[0].name);
   }
 
-  editTodoItem(id: number){
-    this.todoList[id].name = "Wonderhoy"
+  openDialog(enterAnimationDuration: string, exitAnimationDuration: string, id: number): void {
+    this.editedItem.set(this.todoList[id].name);
+    const dialogRef = this.dialog.open(EditTodoModal, {
+      width: '250px',
+      enterAnimationDuration,
+      exitAnimationDuration,
+      data: {id: id, editedItem: this.editedItem()}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result != undefined){
+        this.todoList[id].name = result;
+        console.log(this.todoList);
+        this.cdr.detectChanges();
+      }
+    })
   }
 }
